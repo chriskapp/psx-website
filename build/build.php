2eeb42f0b71323c696b8bb747c31d1cfa637fd1d
Christoph Kappestein <k42b3.x@gmail.com>
2016-04-30 09:52:42 +0200

    add new build file

---
+++ build/build.php
@@ -0,0 +1,56 @@
+<?php
+
+define('DEBUG', false);
+
+$cwd      = getcwd();
+$basePath = __DIR__ . '/..';
+$projects = json_decode(file_get_contents($basePath . '/projects.json'));
+$result   = array_merge((array) $projects->primary, (array) $projects->secondary);
+
+foreach ($result as $project => $row) {
+
+    echo '-- Build ' . $project . "\n";
+
+    // remove existing
+    $apiDir = $basePath . '/public/api/' . $project;
+    if (is_dir($apiDir)) {
+        executeCmd('rm -rf ' . $apiDir);
+    }
+
+    $coverageDir = $basePath . '/public/coverage/' . $project;
+    if (is_dir($coverageDir)) {
+        executeCmd('rm -rf ' . $coverageDir);
+    }
+
+    // clone repo
+    $gitDir = $basePath . '/build/' . $project;
+    executeCmd('git clone ' . $row->git . ' ' . $gitDir);
+
+    if (is_dir($gitDir)) {
+        // install composer
+        chdir($gitDir);
+
+        executeCmd('composer install');
+
+        // generate api doc
+        executeCmd('apigen --source ' . $basePath . '/build/' . $project . '/src --destination ' . $basePath . '/public/api/' . $project);
+
+        // generate code coverage
+        executeCmd('phpunit --coverage-html ' . $basePath . '/public/coverage/' . $project);
+
+        chdir($cwd);
+
+        echo 'Done!' . "\n\n";
+    } else {
+        echo 'Could not clone ' . $row->git . ' into ' . $gitDir . "\n\n";
+    }
+}
+
+function executeCmd($cmd)
+{
+    echo '> ' . $cmd . "\n";
+
+    if (DEBUG === false) {
+        echo shell_exec($cmd) . "\n";
+    }
+}
