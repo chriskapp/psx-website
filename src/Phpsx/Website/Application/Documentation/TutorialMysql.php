74094d2d9af4321dcddf7d4a0f58fc11e3c64187
Christoph Kappestein <k42b3.x@gmail.com>
2016-09-11 00:33:15 +0200

    add mysql tutorial

---
+++ src/Phpsx/Website/Application/Documentation/TutorialMysql.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Phpsx\Website\Application\Documentation;
+
+use PSX\Framework\Controller\ViewAbstract;
+
+class TutorialMysql extends ViewAbstract
+{
+    public function doIndex()
+    {
+    }
+}
